404568840c0832b6bcc50fd8ad060dbd4499cafa
shashwat357 <113761532+shashwat357@users.noreply.github.com>
2023-07-31 13:30:31 +0530

    secondCommit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import  helmet from 'helmet';
+
+
+// Configurations
+
+dotenv.config()
+
+const app=express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+
+// setting routes
+app.use("/client",clientRoutes);
+app.use("/general",generalRoutes);
+app.use("/managment",managmentRoutes);
+app.use("/slaes",salesRoutes);
